226f3775b42df7368e8e6baa868fb437fbbd3189
Sean Porter <portertech@gmail.com>
2014-06-27 10:40:09 -0700

    updated license copyright

--- lib/kitchen/driver/docker.rb
+++ lib/kitchen/driver/docker.rb
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 #
-# Copyright (C) 2013, Sean Porter
+# Copyright (C) 2014, Sean Porter
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
