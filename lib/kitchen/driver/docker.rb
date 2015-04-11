e61e4f38c1cbaeef068a39544eeab36f8b43369d
Sean Porter <portertech@gmail.com>
2015-04-11 15:47:26 -0700

    security-opt=

--- lib/kitchen/driver/docker.rb
+++ lib/kitchen/driver/docker.rb
@@ -234,7 +234,7 @@ module Kitchen
         cmd << " --privileged" if config[:privileged]
         Array(config[:cap_add]).each {|cap| cmd << " --cap-add=#{cap}"} if config[:cap_add]
         Array(config[:cap_drop]).each {|cap| cmd << " --cap-drop=#{cap}"} if config[:cap_drop]
-        Array(config[:security_opt]).each {|opt| cmd << " --security-opt #{opt}"} if config[:security_opt]
+        Array(config[:security_opt]).each {|opt| cmd << " --security-opt=#{opt}"} if config[:security_opt]
         cmd << " #{image_id} #{config[:run_command]}"
         cmd
       end
