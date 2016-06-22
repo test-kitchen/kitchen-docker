f58d170362f7a1c5aa9dd2687dcc45e6a9f6da69
Noah Kantrowitz <noah@coderanger.net>
2016-06-22 16:12:12 -0700

    Clean up the capabilities test suite.

--- test/integration/capabilities/serverspec/capabilities_drop_spec.rb
+++ test/integration/capabilities/serverspec/capabilities_drop_spec.rb
@@ -1,6 +1,23 @@
-require_relative 'spec_helper' 
+#
+# Copyright 2016, Noah Kantrowitz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-describe command('sudo /sbin/ifconfig eth0 multicast') do
-  its(:exit_status) { should_not eq 0 }
-  its(:stdout) { should match /Operation not permitted/ }
+require 'serverspec'
+set :backend, :exec
+
+describe command('ifconfig eth0 multicast') do
+  its(:exit_status) { is_expected.to_not eq 0 }
+  its(:stdout) { is_expected.to match /Operation not permitted/ }
 end
