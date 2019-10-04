ce04a06ff5848bc5fb2b99e2323012370b3a50cc
Michael Geiger <info@mgeiger.de>
2019-10-04 13:13:55 +0200

    suse platform: use system script for ssh key initialization
    Signed-off-by: Michael Geiger <info@mgeiger.de>

--- lib/kitchen/docker/container/linux.rb
+++ lib/kitchen/docker/container/linux.rb
@@ -140,8 +140,7 @@ module Kitchen
                        <<-CODE
                          ENV container docker
                          RUN zypper install -y sudo openssh which curl
-                         RUN [ -f "/etc/ssh/ssh_host_rsa_key" ] || ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
-                         RUN [ -f "/etc/ssh/ssh_host_dsa_key" ] || ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N ''
+                         RUN /usr/sbin/sshd-gen-keys-start
                        CODE
                      when 'arch'
                        # See https://bugs.archlinux.org/task/47052 for why we
