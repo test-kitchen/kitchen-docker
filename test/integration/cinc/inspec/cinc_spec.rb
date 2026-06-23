7d3de5852d313ef04fed3ad356adf8ce09521db2
Corey Hemminger <hemminger@hotmail.com>
2026-06-23 15:38:04 -0500

    fix: use Windows containers for Windows CI integration test
    The Windows CI job was attempting to run Linux containers (ubuntu-24.04)
    on a Windows Docker daemon in Windows container mode, which is impossible.
    The Docker daemon on GHA windows-latest cannot pull Linux images.
    
    Switch the Windows integration test to use actual Windows containers:
    - Add windows-2022 platform using mcr.microsoft.com/windows/servercore:ltsc2022
    - Update workflow matrix to target windows-2022 instead of ubuntu-2404
    - Remove DOCKER_DEFAULT_PLATFORM env (not needed for native Windows containers)
    - Make InSpec cinc-client version check cross-platform
    
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/integration/cinc/inspec/cinc_spec.rb
+++ test/integration/cinc/inspec/cinc_spec.rb
@@ -1,9 +1,21 @@
-describe file("/tmp/cinc-converged") do
+file_path = if os.windows?
+              "C:/cinc-converged"
+            else
+              "/tmp/cinc-converged"
+            end
+
+describe file(file_path) do
   it { should exist }
   its(:content) { should eq "ok\n" }
 end
 
-describe command("/opt/cinc/bin/cinc-client --version") do
+cinc_cmd = if os.windows?
+             'C:\cinc-project\cinc\bin\cinc-client --version'
+           else
+             "/opt/cinc/bin/cinc-client --version"
+           end
+
+describe command(cinc_cmd) do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(/Cinc Client/) }
 end
