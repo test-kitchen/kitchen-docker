a91e7b7fc7f442b9d568d8de999ecf381ac673c9
Corey Hemminger <hemminger@hotmail.com>
2026-06-23 19:27:06 -0500

    tes
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/cookbooks/cinc_test/recipes/default.rb
+++ test/cookbooks/cinc_test/recipes/default.rb
@@ -1,4 +1,10 @@
-file "/tmp/cinc-converged" do
+file_path = if windows?
+              "C:/cinc-converged"
+            else
+              "/tmp/cinc-converged"
+            end
+
+file file_path do
   content "ok\n"
   mode "0644"
 end
